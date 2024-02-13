e8aef9c49bfb62051688e6c0868a11aaac90d93d
JuanGP <juangpc@gmail.com>
2024-02-13 16:13:02 +0100

    add second promises example

--- promises.js
+++ promises.js
@@ -21,4 +21,28 @@ addTwo(1,2)
   .then(num => addTwo(num, 4))
   .then(num => console.log(`Final: ${num}`),
         str => console.log(str))
+// ----------------------------
+// another example
+// ----------------------------
+
+let globalNumber = 0;
+
+function numberPlusNumber(number) {
+  return new Promise((resolve, reject) => {
+    // const number = Math.floor(Math.random() * 10);
+    globalNumber += number;
+    setTimeout(() => {
+      resolve(globalNumber);  
+    }, Math.floor(Math.random() * 1000));
+    
+  }).then((n) => {
+      console.log(`number: ${n}`);
+      return n});
+}
+
+console.log('--------------');
+obtainNumberPlusOne(1)
+  .then((number) => numberPlusNumber(1))
+  .then((number) => numberPlusNumber(2))
+  .then((number) => numberPlusNumber(4))
 
